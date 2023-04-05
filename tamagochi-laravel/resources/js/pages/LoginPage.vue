5e38a86dd6e636fa80e6787e663f822ec8bb27f5
szogipeti <szogipeti02@gmail.com>
2023-04-05 10:28:51 +0200

    Write hungarian error messages

--- tamagochi-laravel/resources/js/pages/LoginPage.vue
+++ tamagochi-laravel/resources/js/pages/LoginPage.vue
@@ -1,18 +1,18 @@
 <template>
     <div id="login" class="mx-auto p-3">
-        <h2>Bejelentkezés</h2>
+        <h3>Bejelentkezés</h3>
         <Form @submit="login" :validation-schema="schema">
             <div class="form-group">
                 <Field class="form-control" type="email" placeholder="E-mail" name="email" />
                 <error-message name="email" />
             </div>
             <div class="form-group">
-                <Field class="form-control" type="password" placeholder="Password" name="password" />
+                <Field class="form-control" type="password" placeholder="Jelszó" name="password" />
                 <error-message name="password" />
             </div>
             <button type="submit" class="btn btn-primary my-2">Bejelentkezés</button>
         </Form>
-        <h5>Nincs még fiókod? <router-link to="">Regisztrálj itt!</router-link></h5>
+        <h5>Nincs még fiókod? <router-link to="/register">Regisztrálj itt!</router-link></h5>
     </div>
 </template>
 
@@ -22,8 +22,15 @@ import {http} from "../utils/http.js"
 import * as yup from "yup"
 
 const schema = yup.object({
-    email: yup.string().email().min(6).max(100).required(),
-    password: yup.string().min(6).max(100).required()
+    email: yup.string()
+        .email('Az e-mail formátuma nem megfelelő!')
+        .min(6, 'Az e-mailnek minimum 6 hosszúnak kell lennie!')
+        .max(100, 'Az e-mail maximum 100 karakter hosszú lehet!')
+        .required('Az e-mail megadása kötelező!'),
+    password: yup.string()
+        .min(6, 'A jelszónak minimum 6 karakter hosszúnak kell lennie!')
+        .max(100, 'A jelszó maximum 100 karakter hosszú lehet')
+        .required('A jelszó megadása kötelező!'),
 });
 
 const login = async function(){
@@ -44,7 +51,6 @@ const login = async function(){
 input{
     margin: 5px auto;
     padding: 10px 5px;
-    border: 2px solid lightgray;
 }
 
 </style>
