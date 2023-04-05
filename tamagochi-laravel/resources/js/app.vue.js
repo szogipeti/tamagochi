79268e7053e4f670c98a3b1a60dcfca22dc62029
szogipeti <szogipeti02@gmail.com>
2023-04-05 09:14:31 +0200

    Create basic navbar

--- tamagochi-laravel/resources/js/app.vue.js
+++ tamagochi-laravel/resources/js/app.vue.js
@@ -4,8 +4,14 @@ import {router} from '@/router/index.js'
 import BSAlert from '@/components/layout/BSAlert.vue'
 import App from '@/App.vue';
 
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+import {faCircleUser} from '@fortawesome/free-solid-svg-icons'
+library.add(faCircleUser)
+
 const app = createApp(App);
 app.use(createPinia());
 app.use(router);
 app.component('ErrorComponent', BSAlert);
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount("#app");
