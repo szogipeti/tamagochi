3d42e3ae7a77f0f0884b733cce2047a0e4b7ff28
denesdonat <donatking2002@gmail.com>
2023-04-05 11:02:35 +0200

    Models

---
+++ tamagochi-laravel/app/Models/Animal_stat.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Animal_stat extends Model
+{
+    protected $fillable = ['animals_id', 'Név', 'Éhség', 'Szomjúság', 'Jókedv','Mozgás','Egészség','Ügyesség'];
+
+    public $timestamps = false;
+    public function animal(){
+        return $this->belongsTo(Animals::class, 'animals_id',"id",);
+    }
+}
