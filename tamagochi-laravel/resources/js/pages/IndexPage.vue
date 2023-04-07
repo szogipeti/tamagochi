2ae4e299c536e60dbdc6d4c53c64fd4e67d01e4f
szogipeti <szogipeti02@gmail.com>
2023-04-07 08:20:59 +0200

    Create redirection from index to login if not logged in

--- tamagochi-laravel/resources/js/pages/IndexPage.vue
+++ tamagochi-laravel/resources/js/pages/IndexPage.vue
@@ -1,5 +1,21 @@
 <template>
+    <div class="card mx-auto my-5 p-3" v-if="!loggedIn.isLoggedIn">
+        <h2 class="card-title my-1">Jelentkezz be, hogy megnézd a háziállatod!</h2>
+        <router-link class="btn btn-primary mx-auto my-1" to="/login">Bejelentkezés</router-link>
+    </div>
 </template>
 
 <script setup>
+import {RouterLink} from 'vue-router';
+import {useLoggedInStore} from "../store/isLoggedIn";
+
+const loggedIn = useLoggedInStore();
 </script>
+<style scoped>
+.card{
+    width: 700px;
+}
+router-link{
+    width: 20em;
+}
+</style>
