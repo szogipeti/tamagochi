0404b1d181fb7930a70577551067c3811301b673
szogipeti <szogipeti02@gmail.com>
2023-04-07 11:53:18 +0200

    Update age live

--- tamagochi-laravel/resources/js/components/StatBox.vue
+++ tamagochi-laravel/resources/js/components/StatBox.vue
@@ -1,41 +1,41 @@
 <template>
-<div class="p-3 m-3">
-    <h3>{{props.name.charAt(0).toUpperCase() + props.name.slice(1)}} adatai</h3>
-    <table class="table table-responsive">
-        <tr>
-            <th>Éhség:</th>
-            <td>{{props.hunger}}</td>
-        </tr>
-        <tr>
-            <th>Szomjúság:</th>
-            <td>{{props.thirst}}</td>
-        </tr>
-        <tr>
-            <th>Jókedv:</th>
-            <td>{{props.happiness}}</td>
-        </tr>
-        <tr>
-            <th>Mozgás:</th>
-            <td>{{props.activity}}</td>
-        </tr>
-        <tr>
-            <th>Egészség:</th>
-            <td>{{props.health}}</td>
-        </tr>
-        <tr>
-            <th>Ügyesség:</th>
-            <td>{{props.dexterity}}</td>
-        </tr>
-        <tr>
-            <th>Kor:</th>
-            <td>{{age}} év</td>
-        </tr>
-    </table>
-</div>
+    <div class="p-3 m-3">
+        <h3>{{ props.name.charAt(0).toUpperCase() + props.name.slice(1) }} adatai</h3>
+        <table class="table table-responsive">
+            <tr>
+                <th>Éhség:</th>
+                <td>{{ props.hunger }}</td>
+            </tr>
+            <tr>
+                <th>Szomjúság:</th>
+                <td>{{ props.thirst }}</td>
+            </tr>
+            <tr>
+                <th>Jókedv:</th>
+                <td>{{ props.happiness }}</td>
+            </tr>
+            <tr>
+                <th>Mozgás:</th>
+                <td>{{ props.activity }}</td>
+            </tr>
+            <tr>
+                <th>Egészség:</th>
+                <td>{{ props.health }}</td>
+            </tr>
+            <tr>
+                <th>Ügyesség:</th>
+                <td>{{ props.dexterity }}</td>
+            </tr>
+            <tr>
+                <th>Kor:</th>
+                <td>{{ age }} év</td>
+            </tr>
+        </table>
+    </div>
 </template>
 
 <script setup>
-import {computed} from "vue";
+import {watch, ref, onMounted} from "vue";
 
 const props = defineProps({
     name: String,
@@ -48,9 +48,37 @@ const props = defineProps({
     created_at: String
 })
 
-const age = computed(() => {
-    return Math.ceil((Date.now() - Date.parse(props.created_at)) / (1000 * 60))
+const age = ref(Math.ceil((Date.now() - Date.parse(props.created_at)) / (1000 * 60)));
+
+const countDown = ref(
+    60 -
+    Math.ceil(
+        (Date.now() - Date.parse(props.created_at)) / (1000) -
+        Math.floor((Date.now() - Date.parse(props.created_at)) / (1000) / 60) * 60
+    )
+);
+
+const timer = function () {
+    if (countDown.value > 0) {
+        setTimeout(() => {
+            countDown.value--;
+            timer();
+        }, 1000)
+    }
+}
+
+watch(countDown, (newCountDown) => {
+    if (newCountDown <= 0) {
+        age.value++;
+        countDown.value = 60;
+        timer();
+    }
 })
+
+onMounted(() => {
+    timer();
+})
+
 </script>
 
 <style scoped>
