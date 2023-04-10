a5d4339dc218e0a3ef73cf906c1348db6f08b79d
szogipeti <szogipeti02@gmail.com>
2023-04-10 16:18:29 +0200

    Create models of database

---
+++ Tamagochi/Tamagochi/Model/PersonalAccessToken.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tamagochi.Model
+{
+    public partial class PersonalAccessToken
+    {
+        public ulong Id { get; set; }
+        public string TokenableType { get; set; } = null!;
+        public ulong TokenableId { get; set; }
+        public string Name { get; set; } = null!;
+        public string Token { get; set; } = null!;
+        public string? Abilities { get; set; }
+        public DateTime? LastUsedAt { get; set; }
+        public DateTime? ExpiresAt { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
