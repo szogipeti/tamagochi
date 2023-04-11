ca78babfcdf63061cc376844125582665eba5e8f
szogipeti <szogipeti02@gmail.com>
2023-04-11 12:56:07 +0200

    Test backend error messages

--- TamagochiSeleniumTests/TamagochiSeleniumTests/RegisterTests.cs
+++ TamagochiSeleniumTests/TamagochiSeleniumTests/RegisterTests.cs
@@ -1,6 +1,4 @@
-﻿
-using System.ComponentModel.Design;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 
 namespace TamagochiSeleniumTests;
 
@@ -29,26 +27,64 @@ public class RegisterTests
         process.WaitForExit();
     }
 
+    public void RegisterUser(string username, string email, string pwd, string pwdConf)
+    {
+        IWebElement usernameElement = chromeDriver.FindElement(By.XPath("//*[@id=\"register\"]/form/div[1]/input"));
+        IWebElement emailElement = chromeDriver.FindElement(By.XPath("//*[@id=\"register\"]/form/div[2]/input"));
+        IWebElement pwdElement = chromeDriver.FindElement(By.XPath("//*[@id=\"register\"]/form/div[3]/input"));
+        IWebElement pwdConfElement = chromeDriver.FindElement(By.XPath("//*[@id=\"register\"]/form/div[4]/input"));
+        
+        usernameElement.SendKeys(username);
+        emailElement.SendKeys(email);
+        pwdElement.SendKeys(pwd);
+        pwdConfElement.SendKeys(pwdConf);
+
+        IWebElement button = chromeDriver.FindElement(By.XPath("//*[@id=\"register\"]/form/button"));
+        button.Click();
+    }
+
+    public void WaitForRegister()
+    {
+        Stopwatch sw = new Stopwatch();
+        sw.Start();
+        while (sw.ElapsedMilliseconds <= 20000)
+        {
+            if (chromeDriver.Url == "http://localhost:8881/#/login")
+            {
+                break;
+            }
+        }
+        sw.Stop();
+    }
+
+    public void WaitForInvalidRegister()
+    {
+        Stopwatch sw = new Stopwatch();
+        sw.Start();
+        while (sw.ElapsedMilliseconds <= 20000)
+        {
+            try
+            {
+                chromeDriver.FindElement(By.XPath("//*[@id=\"register\"]/form/div[5]/div/p"));
+                break;
+            }
+            catch
+            {
+                
+            }
+        }
+        sw.Stop();
+    }
+
     [TestMethod]
     public void TestSuccessfulRegister()
     {
         LoadDatabase();
 
-        IWebElement username = chromeDriver.FindElement(By.XPath("//*[@id=\"register\"]/form/div[1]/input"));
-        IWebElement email = chromeDriver.FindElement(By.XPath("//*[@id=\"register\"]/form/div[2]/input"));
-        IWebElement pwd = chromeDriver.FindElement(By.XPath("//*[@id=\"register\"]/form/div[3]/input"));
-        IWebElement pwdConf = chromeDriver.FindElement(By.XPath("//*[@id=\"register\"]/form/div[4]/input"));
+        RegisterUser("gipszjakab", "gipszjakab@gmail.com", "jelszo123", "jelszo123");
         
-        username.SendKeys("gipszjakab");
-        email.SendKeys("gipszjakab@gmail.com");
-        pwd.SendKeys("jelszo123");
-        pwdConf.SendKeys("jelszo123");
+        WaitForRegister();
 
-        IWebElement button = chromeDriver.FindElement(By.XPath("//*[@id=\"register\"]/form/button"));
-        button.Click();
-        
-        Thread.Sleep(10000);
-        
         Assert.AreEqual("http://localhost:8881/#/login", chromeDriver.Url);
         Assert.AreEqual("Bejelentkezés", chromeDriver.FindElement(By.XPath("//*[@id=\"login\"]/h3")).Text);
     }
@@ -69,19 +105,8 @@ public class RegisterTests
     [TestMethod]
     public void TestInvalidEmailRegister()
     {
-        IWebElement username = chromeDriver.FindElement(By.XPath("//*[@id=\"register\"]/form/div[1]/input"));
-        IWebElement email = chromeDriver.FindElement(By.XPath("//*[@id=\"register\"]/form/div[2]/input"));
-        IWebElement pwd = chromeDriver.FindElement(By.XPath("//*[@id=\"register\"]/form/div[3]/input"));
-        IWebElement pwdConf = chromeDriver.FindElement(By.XPath("//*[@id=\"register\"]/form/div[4]/input"));
-        
-        username.SendKeys("gipszjakab");
-        email.SendKeys("gipszjakab134");
-        pwd.SendKeys("jelszo123");
-        pwdConf.SendKeys("jelszo123");
-        
-        IWebElement button = chromeDriver.FindElement(By.XPath("//*[@id=\"register\"]/form/button"));
-        button.Click();
-        
+        RegisterUser("gipszjakab", "gipszjakab134", "jelszo123", "jelszo123");
+
         Assert.AreEqual("http://localhost:8881/#/register", chromeDriver.Url);
         Assert.AreEqual("Az e-mail formátuma nem megfelelő!", chromeDriver.FindElement(By.XPath("//*[@id=\"register\"]/form/div[2]/span")).Text);
     }
@@ -89,21 +114,46 @@ public class RegisterTests
     [TestMethod]
     public void TestNotMatchingPasswordRegister()
     {
-        IWebElement username = chromeDriver.FindElement(By.XPath("//*[@id=\"register\"]/form/div[1]/input"));
-        IWebElement email = chromeDriver.FindElement(By.XPath("//*[@id=\"register\"]/form/div[2]/input"));
-        IWebElement pwd = chromeDriver.FindElement(By.XPath("//*[@id=\"register\"]/form/div[3]/input"));
-        IWebElement pwdConf = chromeDriver.FindElement(By.XPath("//*[@id=\"register\"]/form/div[4]/input"));
-        
-        username.SendKeys("gipszjakab");
-        email.SendKeys("gipszjakab134");
-        pwd.SendKeys("jelszo123");
-        pwdConf.SendKeys("jelszo1234");
+        RegisterUser("gipszjakab", "gipszjakab@gmail.com", "jelszo123", "jelszo1234");
         
         IWebElement button = chromeDriver.FindElement(By.XPath("//*[@id=\"register\"]/form/button"));
         button.Click();
 
         Assert.AreEqual("http://localhost:8881/#/register", chromeDriver.Url);
         Assert.AreEqual("A jelszó megerősítésének meg kell egyeznie a jelszóval!", chromeDriver.FindElement(By.XPath("//*[@id=\"register\"]/form/div[4]/span")).Text);
+    }
+
+    [TestMethod]
+    public void TestUsedUsernameRegister()
+    {
+        LoadDatabase();
+        
+        RegisterUser("gipszjakab", "gipszjakab@gmail.com", "jelszo123", "jelszo123");
+        WaitForRegister();
+        
+        chromeDriver.Navigate().GoToUrl("http://localhost:8881/#/register");
+        RegisterUser("gipszjakab", "gipszjakab2@gmail.com", "jelszo123", "jelszo123");
+        
+        WaitForInvalidRegister();
+        
+        Assert.AreEqual("A felhasználónév már használatban van!", chromeDriver.FindElement(By.XPath("//*[@id=\"register\"]/form/div[5]/div/p")).Text);
+    }
+
+    [TestMethod]
+    public void TestUsedEmailRegister()
+    {
+        LoadDatabase();
+        
+        RegisterUser("gipszjakab", "gipszjakab@gmail.com", "jelszo123", "jelszo123");
+        WaitForRegister();
+        
+        chromeDriver.Navigate().GoToUrl("http://localhost:8881/#/register");
+        RegisterUser("gipszjakab2", "gipszjakab@gmail.com", "jelszo123", "jelszo123");
+        
+        WaitForInvalidRegister();
+
+        Assert.AreEqual("Az e-mail már használatban van!", chromeDriver.FindElement(By.XPath("//*[@id=\"register\"]/form/div[5]/div/p")).Text);
+
     }
     
     [TestCleanup]
